3f2a663594d43cf21046aab58ce595266d3d8265
Justice Ohene Amofa <70078600+iamamofa@users.noreply.github.com>
2024-05-20 07:55:08 +0000

    expansion huneter  script

---
+++ expansion_hunter_pipeline.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+# Array of the for the file prefixes that are either pathogenic or non-pathogenic files
+
+file_prefixes = (
+    "ERR1955514"
+    "ERR1955398"
+    "ERR1955482" 
+    "ERR1955462"
+    "ERR1955504"
+    "ERR1955527"
+    "ERR1955415"
+    "ERR1955531"
+    "ERR1955473"
+
+)
+
+
+
+# Directories and File Paths that is used ....
+reads_base_directory ="home.dsi-students4/public/project1-expansions"
+variant_catalog_directory ="home.dsi-students4/public/project1-expansions/variant-catalog/grch37/variant_catalog.json"
+reference_directory_file ="home.dsi-students4/public/genomes/GRCh37.fa"
+
+
+#
